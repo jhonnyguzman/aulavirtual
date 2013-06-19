0deb48b168cd5a9883a063387aab9889d6268ddd
Johnny <guzmanjhonny@gmail.com>
2013-06-19 12:44:10 -0300

    agregado orm datamapper al sistema. Modulo de autentificacion de usuarios y permisos funcional 100%

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('cizacl');
+$autoload['libraries'] = array('database','datamapper','cizacl');
 
 
 /*
