b93370f95e4a89cae5d4addcc3173b3f6a1c1b2d
Johnny <guzmanjhonny@gmail.com>
2013-06-20 01:21:54 -0300

    Se agrego vista home

---
+++ application/views/home/index.php
@@ -0,0 +1,128 @@
+<?php echo $this->load->view("default/_header_public"); ?>
+      <div class="row">
+        <div class="span8 content-hero-unit">
+            <!-- Main hero unit for a primary marketing message or call to action -->
+            <div class="hero-unit">
+              <h1>Video here!</h1>
+              <p>Set a video</p>
+              <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+            </div>
+        </div>
+        <div class="span4">
+            <form class="form-signin">
+              <h3 class="form-signin-heading">Registrate</h3>
+              <div class="input-prepend">
+                <span class="add-on"><i class="icon-user"></i></span>
+                <input type="text" class="input-block-level" placeholder="Nombre Completo">
+              </div>
+              <div class="input-prepend">
+                <span class="add-on"><i class="icon-envelope"></i></span>
+                <input type="text" class="input-block-level" placeholder="Correo Electrónico">
+              </div>
+              <div class="input-prepend">
+                <span class="add-on"><i class="icon-lock"></i></span>
+                <input type="password" class="input-block-level" placeholder="Contraseña">
+              </div>
+              
+              <button class="btn btn-mediun btn-primary" type="submit">Registrate</button>
+            </form>
+        </div>
+      </div>
+      <div class="row bar-search">
+            <form action="" method="post" class="form-search-home">
+              <div class="input-append">
+                  <input class="span10" type="text" placeholder="¿Qué quieres aprender hoy?">
+                  <div class="btn-group">
+                    <button type="submit" class="btn btn-success span2">Búsqueda</button>
+                  
+                  </div>
+              </div>
+            </form>
+      </div>
+      <div class="row">
+          <ul class="thumbnails">
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course1.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course2.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course3.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course4.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course5.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <img src="<?=site_url()?>assets/img/test/course6.jpg" alt="">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a href="#" class="btn btn-primary">Action</a> <a href="#" class="btn">Action</a></p>
+                </div>
+              </div>
+            </li>
+          </ul>
+      </div>
+
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+<?php echo $this->load->view("default/_footer_public"); ?>
+
+    
